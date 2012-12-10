2e3a0ee3cb7f8c84b3ccb046b22e06bf72913790
Catalina Bustamante <catalinabustam@gmail.com>
2012-12-10 13:56:55 -0500

    avance formulario

--- application.rb
+++ application.rb
@@ -18,27 +18,48 @@ end
 get '/' do
   @patients = Patient.all
   erb:select_study, locals: {patients: @patients}
+  end
+  
+get '/search' do
+  puts "parametros search"
+  puts params[:id].inspect
+  erb:search
 end
-
+  
 post '/form' do
-  @pat=Patient.get(params[:patient][:id].to_i)
-  rep=@pat.reporte.new(params[:reporte])
-  erb:form, locals: {patient: @pat, reporte: rep}
+  puts params[:reporte]
+  puts params[:patient]
+  @old=Reporte.first(:equipo=>params[:reporte][:equipo], :patient_id=>params[:patient][:id].to_i)
+  puts @old.inspect
+  if @old.nil?
+    $pat=Patient.get(params[:patient][:id].to_i)
+    $rep=$pat.reportes.new(params[:reporte])
+    puts "paciente nuevo"
+    erb:form, locals: {patient: $pat, reporte: $rep} 
+  else 
+    puts "El paciente ya ha sido creado"
+    @pati=Patient.get(@old.patient_id.to_i)
+    erb:update, locals:{report: @old, patient: @pati}
+  end
 end
 
 post '/confirmacion' do
+  puts $rep.inspect
+  @rep = Reporte.create($rep.attributes)
+  @rep.attributes=(params[:reporte])
 
-  @rep = rep
+  puts @rep.inspect
   @rep.esclerosi = Esclerosi.new(params[:esclerosi])
   @rep.displasia = Displasia.new(params[:displasia])
   @rep.malformacion = Malformacion.new(params[:malformacion])
   @rep.tumor = Tumor.new(params[:tumor])
   @rep.conclusion = Conclusion.new(params[:conclusion])
+  @rep.save
 
   if @rep.save
     puts "*******************Report Saved!"
     flash[:notice] = "El reporte se ha guardado satisfactoriamente"
-    erb:confirmation, locals: {reporte: @rep, esclerosi: @rep.esclerosi, displasia: @rep.displasia, malformacion: @rep.malformacion, tumor: @rep.tumor, conclusion: @rep.conclusion}
+    erb:confirmation, locals: {patient: $pat,reporte: @rep, esclerosi: @rep.esclerosi, displasia: @rep.displasia, malformacion: @rep.malformacion, tumor: @rep.tumor, conclusion: @rep.conclusion}
   else
     puts "*******************Patient not saved! "
     flash[:error] = @rep.errors.full_messages.join(",")
@@ -46,6 +67,3 @@ post '/confirmacion' do
   end
 end
 
-post '/Buscar' do
-  erb:search
-end
--- lib/esclerosis.rb
+++ lib/esclerosis.rb
@@ -8,7 +8,9 @@ class Esclerosi
   property :esclerosis_derecha, Boolean
   property :esclerosis_izquierda, Boolean
   property :esclerosis_bilateral, Boolean
-
+  property :hallazgos_extrahipo, Boolean
+  property :descripcion_hallazgos, Text
+  
 
   belongs_to :reporte
 
